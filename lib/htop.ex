169f5f5d0201b586b3339773847bccfaa8f89177
Ross van Zyl <rossvz@gmail.com>
2023-04-26 19:02:27 -0400

    testing

--- lib/htop.ex
+++ lib/htop.ex
@@ -7,5 +7,14 @@ defmodule Htop do
   if it comes from the database, an external API or others.
   """
 
-  def test, do: "Hello"
+  def random_number do
+    Enum.random(1..9999)
+  end
+
+  def my_really_long_function_name(arg) do
+    case arg do
+      i when i < 50 -> IO.inspect("cool")
+      _otherwise -> raise("something went wrong")
+    end
+  end
 end
